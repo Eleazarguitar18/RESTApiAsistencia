682bad01d903b955290f8bc67dc2d3c29150157e
Eleazarguitar18 <eleazarguitar18@gmail.com>
2024-09-05 17:30:38 -0400

    modulos user, clase,asistencia

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { ClaseModule } from './clase/clase.module';
+import { AsistenciaModule } from './asistencia/asistencia.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule, ClaseModule, AsistenciaModule],
   controllers: [AppController],
   providers: [AppService],
 })
