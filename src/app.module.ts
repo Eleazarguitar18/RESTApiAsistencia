99669a40d10fe8e1c352cad176d55555f2ad1a02
Eleazarguitar18 <eleazarguitar18@gmail.com>
2024-09-20 14:01:56 -0400

    creacion y listadeo de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { ClaseModule } from './clase/clase.module';
 import { AsistenciaModule } from './asistencia/asistencia.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ClaseModule, AsistenciaModule],
+  imports: [PrismaModule, UserModule, ClaseModule, AsistenciaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { PrismaService } from "../prisma/prisma.service";
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    const [day, month, year] = createUserDto.Fecha_Nacimiento.split('-');
+    const formattedDate = new Date(`${year}-${month}-${day}`);
+    return this.prisma.usuario.create({
+      data: {
+      Nombre: createUserDto.Nombre,
+      Apellido: createUserDto.Apellido,
+      Fecha_Nacimiento: formattedDate, // Ahora es un Date válido
+      Telefono: createUserDto.Telefono,
+      Email: createUserDto.Email,
+    },})
+    // return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() {
+    const data={
+      status:200,
+      mesasage:`This action returns all user`,
+      data: await this.prisma.usuario.findMany(),
+    }
+    return data;
   }
 
   findOne(id: number) {
